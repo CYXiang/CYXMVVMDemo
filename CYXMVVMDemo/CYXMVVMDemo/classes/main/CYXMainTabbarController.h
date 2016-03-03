0f34dcff58ae64caea19c1987ca3ff281744763a
cyx <applekaifa@go.local>
2016-03-03 16:34:17 +0800

    添加文件

---
+++ CYXMVVMDemo/CYXMVVMDemo/classes/main/CYXMainTabbarController.h
@@ -0,0 +1,13 @@
+//
+//  CYXMainTabbarController.h
+//  CYXMVVMDemo
+//
+//  Created by apple开发 on 16/3/3.
+//  Copyright © 2016年 cyx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CYXMainTabbarController : UITabBarController
+
+@end
---
+++ CYXMVVMDemo/CYXMVVMDemo/classes/main/CYXMainTabbarController.m
@@ -0,0 +1,37 @@
+//
+//  CYXMainTabbarController.m
+//  CYXMVVMDemo
+//
+//  Created by apple开发 on 16/3/3.
+//  Copyright © 2016年 cyx. All rights reserved.
+//
+
+#import "CYXMainTabbarController.h"
+
+@interface CYXMainTabbarController ()
+
+@end
+
+@implementation CYXMainTabbarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
